96444718bd136f648bc423ddddc5d4390963840a
Thomas Dutta <thomasdutta.gmail.com>
2017-08-12 16:01:20 -0500

    enable security

--- src/main/java/com/example/spring/controller/ClientController.java
+++ src/main/java/com/example/spring/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.example.spring.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by thomasdutta on 8/11/17.
  */
 @RestController
+@RefreshScope
 public class ClientController {
 
     @Value("${username: Thomas}")
